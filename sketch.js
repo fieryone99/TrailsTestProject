72bce44c6d53dc6b876d40e5fcdfa466fcffa3eb
fieryone <128168941+fieryone99@users.noreply.github.com>
2024-05-10 14:06:16 +0200

    adding all files to save

---
+++ sketch.js
@@ -0,0 +1,39 @@
+
+let maxLife = 100;
+let angle = 0;
+let maxLen = 200;
+let scaler = 2;
+let listOfParticles = [];
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+  colorMode(HSB, 360, 100, 100, 100, 100);
+}
+
+function draw() {
+  background(0);
+  translate(width/2, height/2);
+
+  for (let i = 0; i < 2; i++) {
+
+  r = maxLen * sin(scaler * angle);
+
+  let x = r * cos(angle);
+  let y = r * sin(angle);
+
+  let p = new Particle(x, y);
+  listOfParticles.push(p);
+
+  for (let i = 0; i < listOfParticles.length; i++) {
+      let particle = listOfParticles[i];
+      particle.show();
+      particle.update();
+      if (particle.lifespan < 0) {
+        listOfParticles.splice(i, 1); 
+      }
+
+    }
+
+    angle += 0.01;
+  }
+}
+
